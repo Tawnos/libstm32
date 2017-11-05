2c9978ea360f1400e8bcab0438970b33fcbb1ed4
cmd <cmdc0dez@gmail.com>
2017-11-04 21:01:12 -0700

    adding support for stm32l4xx

--- config.h
+++ config.h
@@ -2,10 +2,15 @@
 #define LIBSTM32_CONFIG_H
 
 #ifdef STM32F10X_MD
-#include <stm32f1xx_hal.h>
-#include <stm32f1xx_hal_gpio.h>
-#include <stm32f1xx_hal_tim.h>
-#include <stm32f1xx_hal_tim_ex.h>
+	#include <stm32f1xx_hal.h>
+	#include <stm32f1xx_hal_gpio.h>
+	#include <stm32f1xx_hal_tim.h>
+	#include <stm32f1xx_hal_tim_ex.h>
+#elif defined (STM32L452xx)
+	#include <stm32l4xx_hal.h>
+	#include <stm32l4xx_hal_gpio.h>
+	#include <stm32l4xx_hal_tim.h>
+	#include <stm32l4xx_hal_tim_ex.h>
 #endif
 
 #endif
