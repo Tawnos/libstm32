a81a4ca89d31fa394a992ff5ed50ed7f05b024eb
cmd <cmdc0dez@gmail.com>
2018-05-07 02:06:31 -0700

    new display message type

---
+++ app/display_message_state.cpp
@@ -0,0 +1,33 @@
+#include "display_message_state.h"
+#include "../display/display_device.h"
+
+using namespace cmdc0de;
+
+//=======================================================================
+DisplayMessageState::DisplayMessageState() :
+		TimeInState(DEFAULT_TIME_IN_STATE), NextState(0), Display(0) {
+}
+
+DisplayMessageState::~DisplayMessageState() {
+}
+
+ErrorType DisplayMessageState::onInit() {
+	getDisplay().fillScreen(RGBColor::BLACK);
+	return ErrorType();
+}
+
+void DisplayMessageState::setMessage(const char *msg) {
+	strncpy(&this->Message[0], msg, sizeof(this->Message));
+}
+
+StateBase::ReturnStateContext DisplayMessageState::onRun() {
+	getDisplay().drawString(0, 10, &this->Message[0], RGBColor::WHITE, RGBColor::BLACK, 1, true);
+	if (timeInState() > TimeInState) { //|| kb.getLastKeyReleased() != QKeyboard::NO_PIN_SELECTED) {
+		return ReturnStateContext(getNextState());
+	}
+	return ReturnStateContext(this);
+}
+
+ErrorType DisplayMessageState::onShutdown() {
+	return ErrorType();
+}
---
+++ app/display_message_state.h
@@ -0,0 +1,44 @@
+#ifndef CMDC0DE_DISPLAY_MESSAGE_STATE_H
+#define CMDC0DE_DISPLAY_MESSAGE_STATE_H
+
+#include "state_base.h"
+
+namespace cmdc0de {
+
+class DisplayDevice;
+
+class DisplayMessageState: public StateBase {
+public:
+	static const uint16_t DEFAULT_TIME_IN_STATE = 3000;
+public:
+	DisplayMessageState();
+	virtual ~DisplayMessageState();
+	void setMessage(const char *msg);
+	void setTimeInState(uint16_t t) {
+		TimeInState = t;
+	}
+	void setNextState(StateBase *b) {
+		NextState = b;
+	}
+	StateBase *getNextState() {
+		return NextState;
+	}
+	void setDisplay(DisplayDevice *dd) {Display = dd;}
+	//const DisplayDevice *getDisplay() const {return Display;}
+	DisplayDevice &getDisplay() {return *Display;}
+	const StateBase *getNextState() const {return NextState;}
+protected:
+	virtual ErrorType onInit();
+	virtual ReturnStateContext onRun();
+	virtual ErrorType onShutdown();
+private:
+	char Message[64];
+	uint16_t TimeInState;
+	StateBase *NextState;
+	DisplayDevice *Display;
+};
+
+
+}
+
+#endif
